d12692e363ff1f5f888fdd9c89ed62af5925d2d4
povoa.fl <fabricio@ctprime.com.br>
2019-10-06 21:30:39 -0300

    Init

---
+++ src/main/java/com/fbrc/slradmin/SlradminApplication.java
@@ -0,0 +1,13 @@
+package com.fbrc.slradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlradminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlradminApplication.class, args);
+	}
+
+}
