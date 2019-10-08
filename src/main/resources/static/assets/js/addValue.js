bcb907057d59fa3d301398d963b90ce7cb72b202
Fabricio Lemes Povoa <fabricio.povoa@goias.gov.br>
2019-10-08 16:32:06 -0300

    Add

--- src/main/resources/static/assets/js/addValue.js
+++ src/main/resources/static/assets/js/addValue.js
@@ -1,3 +1,24 @@
+var arr = [];
+
 function addValue() {
-	alert('Fabricio')
+	
+	var field = document.getElementById("myField").value;
+	var value = document.getElementById("myValue").value;
+	
+	var obj = {"field":"", "value":""};
+	
+	obj.field=field;
+	obj.value=value;
+	arr.push(obj);
+	
+	var text = "";
+	for (i = 0; i < arr.length; i++) {
+		  text += JSON.stringify(arr[i]) + '<br/>';
+		}
+	document.getElementById("previewObj").innerHTML = text;
+}
+
+function clearCollection() {
+	arr = [];
+	document.getElementById("previewObj").innerHTML = "";
 }
